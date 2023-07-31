5dba48a738cbcc9fa2f2d9d3545ce0056e35fc25
doodleoo <106701127+doodleoo3@users.noreply.github.com>
2023-07-31 12:03:58 +0300

    Update send_mainnet_tran.py

--- tran/send_mainnet_tran.py
+++ tran/send_mainnet_tran.py
@@ -1,18 +1,14 @@
 from typing import Optional
 from hexbytes import HexBytes
 from web3 import Web3
-from src.config import MNEMONIC
-from src.config import private_key
+from src.config import PRIVATE_KEY
+from src.config import FAUCET_ADDRESS
 
 
 def send_mainnet_tran(user_address):
-    faucet_address = '0xf1782c522287b87Ce9c4545FeEA43DF26D28ef9E'  
-
     mainnet_rpc_url = "https://haqq-network.rpc.thirdweb.com"
     web3 = Web3(Web3.HTTPProvider(mainnet_rpc_url))
-    
     web3.eth.account.enable_unaudited_hdwallet_features()
-    account = web3.eth.account.from_mnemonic(MNEMONIC)
 
     def build_txn(
     *,
@@ -40,12 +36,12 @@ def send_mainnet_tran(user_address):
 
     transaction = build_txn(
     web3=web3,
-    from_address=faucet_address,
+    from_address=FAUCET_ADDRESS,
     to_address=user_address,
     amount=0.001,
     )
 
-    signed_txn = web3.eth.account.sign_transaction(transaction, private_key)
+    signed_txn = web3.eth.account.sign_transaction(transaction, PRIVATE_KEY)
 
     txn_hash = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
     return f'https://explorer.haqq.network/tx/{txn_hash.hex()}'
